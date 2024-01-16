891ec17955043b691eed1053afb04695fbb6084f
b221b <titarenkomiroslav61@gmail.com>
2024-01-16 12:41:36 +0300

    123

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+// Добавьте необходимую логику обработчиков событий
+function toggleSearch() {
+    var searchInput = document.getElementById('search-input');
+    // ... Остальная логика toggleSearch
+}
+
+function togglePlay() {
+    // ... Остальная логика togglePlay
+}
+
+function prevTrack() {
+    // ... Остальная логика prevTrack
+}
+
+function nextTrack() {
+    // ... Остальная логика nextTrack
+}
+
+function muteUnmute() {
+    // ... Остальная логика muteUnmute
+}
+function showVolumeSlider() {
+    document.querySelector('.volume-bar').style.display = 'block';
+}
+
+function hideVolumeSlider() {
+    document.querySelector('.volume-bar').style.display = 'none';
+}
